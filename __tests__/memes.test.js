7bfdb914a239576721f90fbdeedfe01178606954
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-02 17:02:26 -0700

    failing test for post meme route

--- __tests__/memes.test.js
+++ __tests__/memes.test.js
@@ -18,5 +18,22 @@ describe('app routes', () => {
     return mongoose.connection.close();
   });
 
-  
+  it('creates a meme' () => {
+    return request(app)
+      .post('/api/v1/memes')
+      .send({
+        top: 'My instructor went to topeka',
+        image: '../../assets/facebook-server-farm-arctic-lule-sweden-12.jpg',
+        bottom: '...and all I got was this stupid meme.'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          top: 'My instructor went to topeka',
+          image: '../../assets/facebook-server-farm-arctic-lule-sweden-12.jpg',
+          bottom: '...and all I got was this stupid meme.',
+          __v: 0
+        });
+      })
+  });
 });
